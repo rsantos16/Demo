5d1ac9038a5377a7316e2ae42ba76447fdc5c6a8
= <=>
2012-05-14 19:24:07 -0700

    added some styling

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
 
   subject { @user}
 
-  it {should respond_to(:name)}
+  it {should respond_to (:name)}
   it {should respond_to (:email)}
   it {should respond_to (:password_digest)}
   it {should respond_to (:password)}
