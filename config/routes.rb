1cbd5ddee1b6d264e6eb1bc1069af2b60ca46c2b
= <=>
2012-05-23 21:42:19 -0700

    added sign in stub

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Demo::Application.routes.draw do
 
   resources :users;
+  resources :sessions, only: [:new, :create, :destroy]
  
   root :to => 'static_pages#home'
 
@@ -8,6 +9,9 @@ Demo::Application.routes.draw do
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
